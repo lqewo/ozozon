2ef5ae9ef674981ca89fbfe5fe76e71d0b50a635
lqewo <vrkonosha@gmail.com>
2022-06-08 11:23:12 +0300

    added header and wash section

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,29 @@
 import * as flsFunction from "./modules/functions.js";
 
+
 flsFunction.isWebp();
 
  import Swiper, { Navigation, Pagination } from 'swiper';
 
   const swiper = new Swiper();
+
+
+    $(function() {
+        $('select').styler();
+
+        $('.header__burger').on('click', function (e) {
+          e.preventDefault()
+          $('.header__menu, .header__burger, .header__menu-item' ).toggleClass('active');
+
+          $('body').toggleClass('lock');
+
+          })
+
+          $('.wash__tab-item').on('click', function (e){
+            e.preventDefault();
+            $('.wash__tab-item').removeClass('active');
+            $('.wash__img').removeClass('wash__img--active');
+            $($(this).attr('href')).addClass('wash__img--active');
+            $(this).addClass('active');
+          })
+    });
